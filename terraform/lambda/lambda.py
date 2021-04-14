03027a934dbacce76a608ab62669cae9ba5c4736
Anand Mallik <sankho.mallik@gmail.com>
2021-04-14 11:29:37 -0400

    conforming to pylint

--- terraform/lambda/lambda.py
+++ terraform/lambda/lambda.py
@@ -1,30 +1,31 @@
-import os
 import urllib
 from string import Template
 
-def lambda_handler(event, context):
+def lambda_handler(event, _context):
     with open("./new-account-creation.template.html") as file:
-        fileContents = file.read()
+        file_contents = file.read()
 
     code = urllib.parse.quote(event["request"]["codeParameter"], safe = "")
     username = event["request"]["usernameParameter"]
 
-    template = Template(fileContents)
-    emailMessage = template.substitute({
+    template = Template(file_contents)
+    email_message = template.substitute({
         'code': code,
         'username': username
     })
 
-    smsMessageTemplate = Template("Please visit https://app.pennsieve.net/invitation/accept/${username}/${code}"
-    smsMessage = smsMessageTemplate.substitute({
+    sms_message_template = Template(
+        "Please visit https://app.pennsieve.net/invitation/accept/${username}/${code}"
+    )
+    sms_message = sms_message_template.substitute({
         'code': code,
         'email': username
     })
 
     response = {
-        smsMessage = smsMessage
-        emailSubject = "Welcome to Pennsieve - setup your account"
-        emailMessage = emailMessage
+        "smsMessage": sms_message,
+        "emailSubject": "Welcome to Pennsieve - setup your account",
+        "emailMessage": email_message
     }
 
     return response
