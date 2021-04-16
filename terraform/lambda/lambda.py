834e7e32a77a7a27a7a398d7123437bd2f1b875c
Anand Mallik <sankho.mallik@gmail.com>
2021-04-16 14:16:49 -0400

    sets up password reset to use lambda

--- terraform/lambda/lambda.py
+++ terraform/lambda/lambda.py
@@ -6,8 +6,41 @@ from string import Template
 def lambda_handler(event, _context):
     if event["triggerSource"] == "CustomMessage_AdminCreateUser":
         return handle_admin_create_user(event)
+    elif event["triggerSource"] == "CustomMessage_ForgotPassword":
+        return handle_forgot_password(event)
     else:
         return event
+
+
+def handle_forgot_password(event):
+    with open("./password-reset.template.html") as file:
+        file_contents = file.read()
+
+    code = event["request"]["codeParameter"]
+    username = event["userName"]
+
+    template = Template(file_contents)
+    email_message = template.substitute({
+        'code': code,
+        'username': username,
+        'domain': os.environ.get("PENNSIEVE_DOMAIN")
+    })
+
+    sms_message_template = Template(
+        "Please visit https://app.${domain}/password?verificationCode=${code}"
+    )
+    sms_message = sms_message_template.substitute({
+        'code': code,
+        'username': username,
+        'domain': os.environ.get("PENNSIEVE_DOMAIN")
+    })
+
+    event["response"]["smsMessage"] = sms_message
+    event["response"]["emailSubject"] = "Pennsieve - Password Reset"
+    event["response"]["emailMessage"] = email_message
+
+    return event
+
 
 
 def handle_admin_create_user(event):
