64cf5aba071d090c55fa0c2aaa907ea063a90fa0
Michael Uftring <michael.uftring@pennmedicine.upenn.edu>
2021-11-15 11:04:10 -0500

    add systems manager parameters for postgres access

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,10 @@ variable "environment_name" {}
 variable "service_name" {}
 
 variable "vpc_name" {}
+
+# Postgres
+variable "pennsieve_postgres_host" {}
+
+variable "pennsieve_postgres_db" {
+  default = "pennsieve_postgres"
+}
