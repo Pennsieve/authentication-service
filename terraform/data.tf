7d3b12cb18079946209595b6e6fd70d79e57e778
Michael Uftring <michael.uftring@pennmedicine.upenn.edu>
2021-11-15 10:37:38 -0500

    add access to terraform remote state for platform infrastructure and vpc

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,3 +1,6 @@
+data "aws_region" "current_region" {
+}
+
 # Import Region Data
 data "terraform_remote_state" "region" {
   backend = "s3"
@@ -16,5 +19,28 @@ data "terraform_remote_state" "account" {
     bucket = "${var.aws_account}-terraform-state"
     key    = "aws/terraform.tfstate"
     region = "us-east-1"
+  }
+}
+
+# Import Platform Infrastructure Data
+data "terraform_remote_state" "platform_infrastructure" {
+  backend = "s3"
+
+  config = {
+    bucket = "${var.aws_account}-terraform-state"
+    key    = "aws/${data.aws_region.current_region.name}/${var.vpc_name}/${var.environment_name}/platform-infrastructure/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# Import VPC Data
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket  = "${var.aws_account}-terraform-state"
+    key     = "aws/${data.aws_region.current_region.name}/${var.vpc_name}/terraform.tfstate"
+    region  = "us-east-1"
+    profile = var.aws_account
   }
 }
